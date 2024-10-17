006d04b76357ab0f8abfd5dab95c15d93b1eb00e
john kenneth dalisay <johnkennethdalisay9@gmail.com>
2024-10-18 00:51:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [todos, setTodos] = useState([]);
+    const [task, setTask] = useState('');
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = async () => {
+        const res = await axios.get('http://localhost:5000/todos');
+        setTodos(res.data);
+    };
+
+    const addTodo = async () => {
+        if (!task) return;
+        const res = await axios.post('http://localhost:5000/todos', { task });
+        setTodos([...todos, res.data]);
+        setTask('');
+    };
+
+    const deleteTodo = async (id) => {
+        await axios.delete(`http://localhost:5000/todos/${id}`);
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    const toggleComplete = async (todo) => {
+        const updatedTodo = { ...todo, completed: !todo.completed };
+        await axios.put(`http://localhost:5000/todos/${todo.id}`, updatedTodo);
+        setTodos(todos.map(t => (t.id === todo.id ? updatedTodo : t)));
+    };
+
+    return (
+        <div>
+            <h1>To-Do List</h1>
+            <input 
+                type="text" 
+                value={task} 
+                onChange={(e) => setTask(e.target.value)} 
+                placeholder="Add a new task"
+            />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <span 
+                            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }} 
+                            onClick={() => toggleComplete(todo)}
+                        >
+                            {todo.task}
+                        </span>
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
